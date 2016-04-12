0d46d18c7d2dbd4013c2f8963bbc6f2beb9a6e07
Austin Cory Bart <acbart@vt.edu>
2016-04-12 09:07:32 -0400

    whatever

--- test.py
+++ test.py
@@ -5,11 +5,15 @@ from timeit import default_timer
 root = 'datasets/python/'
 datasets = os.listdir(root)
 
+skip_list = ['baseball', 'nfl', 'soccer', 'twitter', 'gutenberg', 'weather', 'airports', 'johnsongrass']
+
 start_time = default_timer()
 
 os.chdir(root)
 total_datasets = 0
 for dataset in datasets:
+    if dataset in skip_list:
+        continue
     if not os.path.isdir(dataset):
         continue
     python_file = dataset+'.py'
@@ -19,5 +23,6 @@ for dataset in datasets:
     subprocess.call([r"C:\Users\acbart\Anaconda3\python.exe", python_file, "-t"])
     os.chdir('..')
     total_datasets += 1
+    print "\n"
 print("Total datasets: {}".format(total_datasets))
 print("Time taken: {}".format(default_timer() - start_time))
